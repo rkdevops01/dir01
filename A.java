8e05d9b64255dd3aee4eb35bc284ac5f693e5bae
rkdevops01 <rkdevops01@gmail.com>
2023-04-15 13:35:07 +0530

    A.java added

---
+++ A.java
@@ -0,0 +1,18 @@
+class  A1
+{	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+class  A2
+{	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+class  A3
+{	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
